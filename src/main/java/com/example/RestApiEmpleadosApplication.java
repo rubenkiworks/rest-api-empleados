888d6836f1e44e39f84f053ccd00331caf553ee6
rubenkiworks <rubengomezkiworks@gmail.com>
2024-11-19 16:57:42 +0100

    inicializado proyecto

---
+++ src/main/java/com/example/RestApiEmpleadosApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiEmpleadosApplication.class, args);
+	}
+
+}
